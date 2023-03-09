2481f0f00ce442a75c10c0560ed9cd3fce78060a
Marcel Siegel <marcel@siegelnet.de>
2023-03-09 22:15:30 +0100

    spelling corrections

--- react/app/index.jsx
+++ react/app/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Games from './components/Games.jsx';
 
 ReactDOM.render(
-    <Games url="https://api.openligadb.de/getmatchdata/bl1/2016/33"/>,
+    <Games url="https://api.openligadb.de/getmatchdata/bl1/2022/15"/>,
     document.getElementById('app')
 );
